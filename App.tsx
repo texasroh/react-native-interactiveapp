6f047d7f86cd70010bb47edde02fd489caae6c58
June Roh <june.roh@streetsmarket.com>
2022-11-28 17:18:57 -0500

    infinite card stack

--- App.tsx
+++ App.tsx
@@ -1,77 +1,155 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useRef, useState } from "react";
 import {
-  Animated,
-  Dimensions,
-  Easing,
-  PanResponder,
-  Pressable,
-  TouchableOpacity,
+    Animated,
+    PanResponder,
+    Platform,
+    View,
+    Dimensions,
 } from "react-native";
 import styled from "styled-components/native";
+import { Ionicons } from "@expo/vector-icons";
+import icons from "./icons";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: #00a8ff;
 `;
 
-const Box = styled.View`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
+const Card = styled(Animated.createAnimatedComponent(View))`
+    background-color: white;
+    width: 200px;
+    height: 200px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 12px;
+    box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
+    position: absolute;
 `;
 
-const AnimatedBox = Animated.createAnimatedComponent(Box);
+const Btn = styled.TouchableOpacity`
+    margin: 0px 10px;
+`;
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+const BtnContainer = styled.View`
+    flex-direction: row;
+    flex: 1;
+`;
+
+const CardContainer = styled.View`
+    flex: 3;
+    justify-content: center;
+    align-items: center;
+`;
 
 export default function App() {
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x: 0,
-      y: 0,
-    })
-  ).current;
+    const scale = useRef(new Animated.Value(1)).current;
+    const position = useRef(new Animated.Value(0)).current;
+    const rotation = position.interpolate({
+        inputRange: [-400, 400],
+        outputRange: ["-300deg", "300deg"],
+    });
+    const secondScale = position.interpolate({
+        inputRange: [-300, 0, 300],
+        outputRange: [1, 0.5, 1],
+        extrapolate: "clamp",
+    });
+    const onPressIn = Animated.spring(scale, {
+        toValue: 0.95,
+        useNativeDriver: true,
+    });
+    const onPressOut = Animated.spring(scale, {
+        toValue: 1,
+        useNativeDriver: true,
+    });
 
-  const borderRadius = POSITION.y.interpolate({
-    inputRange: [-200, 200],
-    outputRange: [100, 0],
-  });
-  const bgColor = POSITION.y.interpolate({
-    inputRange: [-200, 200],
-    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
-  });
+    const goCenter = Animated.spring(position, {
+        toValue: 0,
+        useNativeDriver: true,
+    });
 
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (_, { dx, dy }) => {
-        POSITION.setValue({ x: dx, y: dy });
-      },
-      onPanResponderRelease: () => {
-        POSITION.flattenOffset();
-      },
-      onPanResponderGrant: () => {
-        POSITION.setOffset({
-          x: POSITION.x._value,
-          y: POSITION.y._value,
-        });
-      },
-    })
-  ).current;
-  POSITION.addListener(() => console.log(POSITION.x, POSITION.y));
-  return (
-    <Container>
-      <StatusBar style="light" />
-      <AnimatedBox
-        {...panResponder.panHandlers}
-        style={{
-          transform: POSITION.getTranslateTransform(),
-          borderRadius,
-          backgroundColor: bgColor,
-        }}
-      />
-    </Container>
-  );
+    const goLeft = Animated.spring(position, {
+        toValue: -500,
+        useNativeDriver: true,
+        tension: 5,
+        restSpeedThreshold: 300,
+        restDisplacementThreshold: 300,
+    });
+
+    const goRight = Animated.spring(position, {
+        toValue: 500,
+        useNativeDriver: true,
+        tension: 5,
+    });
+
+    const panResponder = useRef(
+        PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderGrant: () => onPressIn.start(),
+            onPanResponderRelease: (_, { dx }) => {
+                if (dx < -SCREEN_WIDTH / 3) {
+                    goLeft.start(onDismiss);
+                } else if (dx > SCREEN_WIDTH / 3) {
+                    goRight.start(onDismiss);
+                } else {
+                    Animated.parallel([onPressOut, goCenter]).start();
+                }
+            },
+            onPanResponderMove: (_, { dx }) => {
+                position.setValue(dx);
+            },
+        })
+    ).current;
+
+    const [index, setIndex] = useState(0);
+    const onDismiss = () => {
+        scale.setValue(1);
+        position.setValue(0);
+        setIndex((prev) => prev + 1);
+    };
+
+    const closePress = () => {
+        goLeft.start(onDismiss);
+    };
+    const checkPress = () => {
+        goRight.start(onDismiss);
+    };
+    return (
+        <Container>
+            <StatusBar style="light" />
+            <CardContainer>
+                <Card style={{ transform: [{ scale: secondScale }] }}>
+                    <Ionicons
+                        name={icons[index + 1]}
+                        color="#192a56"
+                        size={98}
+                    />
+                </Card>
+                <Card
+                    {...panResponder.panHandlers}
+                    style={{
+                        transform: [
+                            { scale },
+                            { translateX: position },
+                            { rotate: rotation },
+                        ],
+                    }}
+                >
+                    <Ionicons name={icons[index]} color="#192a56" size={98} />
+                </Card>
+            </CardContainer>
+            <BtnContainer>
+                <Btn onPress={closePress}>
+                    <Ionicons name="close-circle" color="white" size={58} />
+                </Btn>
+                <Btn onPress={checkPress}>
+                    <Ionicons name="checkmark-circle" color="white" size={58} />
+                </Btn>
+            </BtnContainer>
+        </Container>
+    );
 }
