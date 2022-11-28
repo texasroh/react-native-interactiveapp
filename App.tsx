324c566c181c2abeb79bf186c0d529c0f5689c4b
June Roh <texasroh@gmail.com>
2022-11-27 23:36:06 -0500

    animation and interpolation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from "react";
+import { Animated, Easing, Pressable, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.Pressable`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
+  const [up, setUp] = useState(true);
+  const Y_POSITION = useRef(new Animated.Value(-200)).current;
+  const toggleUp = () => setUp((prev) => !prev);
+  const moveUp = () => {
+    Animated.timing(Y_POSITION, {
+      toValue: up ? 200 : -200,
+      useNativeDriver: false,
+      duration: 1000,
+    }).start(toggleUp);
+  };
+  const rotation = Y_POSITION.interpolate({
+    inputRange: [-200, 200],
+    outputRange: ["-360deg", "360deg"],
+  });
+  const borderRadius = Y_POSITION.interpolate({
+    inputRange: [-200, 200],
+    outputRange: [100, 0],
+  });
+  const bgColor = Y_POSITION.interpolate({
+    inputRange: [-200, 200],
+    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <AnimatedBox
+        onPress={moveUp}
+        style={{
+          transform: [{ translateY: Y_POSITION }, { rotateY: rotation }],
+          borderRadius,
+          backgroundColor: bgColor,
+        }}
+      />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
